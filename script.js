e4995408bdf007dba7d5212640353f6daf3c829a
guilhermemendesmoraes-sys <guilherme.mendes.moraes@escola.pr.gov.br>
2026-06-04 21:50:52 -0300

    java

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-const CACHE_NAME = 'limpapp-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
 let textoUltimoAlerta = "";
 let laudoAtual = ""; 
 let historicoTestes = [];
